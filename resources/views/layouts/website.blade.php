d8c708e91e5f9773bc92205f13c8c08fb96083db
wesleyxl <wesley.alvesxll@gmail.com>
2022-04-18 09:10:09 -0300

    establishing structure

---
+++ resources/views/layouts/website.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en, pt-br">
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- end meta tags -->
+
+    <!-- links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap-grid.min.css"
+        integrity="sha512-Xj2sd25G+JgJYwo0cvlGWXoUekbLg5WvW+VbItCMdXrDoZRWcS/d40ieFHu77vP0dF5PK+cX6TIp+DsPfZomhw=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css"
+        integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::to('/asset/website/css/layout.css') }}">
+    <!-- end links -->
+
+    <title>@yield('title')</title>
+
+</head>
+<body>
+
+    <!-- header -->
+    <header></header>
+    <!-- end header -->
+
+    <!-- content --->
+    <main>
+        @yield('content')
+    </main>
+    <!-- end content -->
+
+    <!-- footer -->
+    <footer></footer>
+    <!-- end footer -->
+
+    <!-- scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ URL::to('/asset/website/js/main.js') }}"></script>
+    <!-- end scripts -->
+
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layputs.website')
+@section('title', 'Início')
+@section('content')
+
+    <!-- links -->
+    <link rel="stylesheet" href="{{ URL::to('/assets/website/css/home.css') }}">
+    <!-- end links -->
+
+@endsection
